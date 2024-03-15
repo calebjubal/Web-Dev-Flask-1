f4b96d329832d496f24c87bab393e4fb600b9103
BB9Kairo <85286527+calebjubal@users.noreply.github.com>
2024-03-15 20:12:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '<h1>Pokemon</h1>'
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/search/<id_or_name_of_pokemon>')
+def search_pokemon(id_or_name_of_pokemon):
+    api_url = f"https://api.pokemon.project.projectrexa.dedyn.io/pokeapi/{id_or_name_of_pokemon}?authorization=622BEB8354BCDC1C94E1B5B414C66"
+    response = requests.get(api_url)
+    
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return render_template('pokemon.html', pokemon=pokemon_data)
+    else:
+        return f"Error: {response.status_code}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
